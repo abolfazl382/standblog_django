5ca4985a54c1084acffd671124db3acc1c81149a
Abolfazl <abolfazlnouri17@gmail.com>
2025-07-09 22:57:19 +0330

    add account app

---
+++ account/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    else:
+        return render(request, 'account/login.html')
