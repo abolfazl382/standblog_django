1eaf779e7048d91d3fcb61919f1455cb09de50f3
Abolfazl <abolfazlnouri17@gmail.com>
2025-07-11 16:02:13 +0330

    blog model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/articles')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.body[:30]} ..."
