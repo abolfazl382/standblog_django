cb0f9e11b5fd47a9ad948e258e24f0554bb43349
Abolfazl <abolfazlnouri17@gmail.com>
2025-07-14 11:15:04 +0330

    category added

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
     title = models.CharField(max_length=100)
     body = models.TextField()
     image = models.ImageField(upload_to='images/articles')
